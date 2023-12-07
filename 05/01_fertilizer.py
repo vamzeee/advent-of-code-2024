6ed493f7fe0b30de3bdd5d21e1326d7fbd33a531
Vamsee <44609815+vamzeee@users.noreply.github.com>
2023-12-08 00:24:22 +0530

    added README.md

---
+++ 05/01_fertilizer.py
@@ -0,0 +1,13 @@
+# https://adventofcode.com/2023/day/5
+
+input = open("D:/advent-of-code-2024/05/input.txt").read().strip()
+
+seeds = input.splitlines()
+# .split(': ')[1].split()
+maps = []
+
+kek = input.split('\n\n')
+kek2 = input.split('\n')
+print(kek)
+print(kek2)
+print(seeds)
---
+++ README.md
@@ -0,0 +1,4 @@
+# my solutions for advent of code 2023 - https://adventofcode.com/
+# files are sorted by days
+# if a file is incomplete, it means that I have not managed to solve that problem yet
+# links for the problems are present at the top of every file
